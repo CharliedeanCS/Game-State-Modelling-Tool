aa29955582e9f4800f417e79f9d932b179772ac4
Charlie Dean <47223319+CharliedeanCS@users.noreply.github.com>
2023-02-15 14:56:47 +0000

    Delete Example 3.py

--- Example 3.py
+++
@@ -1,9 +0,0 @@
-from my_states import StateController
-device = StateController()
-device.read_file("Person.json")
-
-device.start_event()
-
-device.load_game_state()
-
-print(device.current_state())
